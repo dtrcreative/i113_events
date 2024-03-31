0ce47cde0a0c081ceb10b2e86a09406ab1841580
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-03-31 12:17:33 +0300

    updated: sh

--- i113_events_update.sh
+++ i113_events_update.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-readonly service_name=i113_events
-readonly service_image_name=events-server
+readonly container_name=i113_events
+readonly image_name=events-server
 
-echo $service_name
-echo $service_image_name
+echo $container_name
+echo $image_name
 
 docker --version
 
-if docker ps | grep -- $service_name > /dev/null
+if docker ps | grep -- $container_name > /dev/null
 then
-docker stop $service_name
-docker rm $service_name
-docker rmi $service_image_name
+    docker stop $container_name
+    docker rm $container_name
+    docker rmi $image_name
 else
-echo no such container
+    echo no such container
 fi
 
